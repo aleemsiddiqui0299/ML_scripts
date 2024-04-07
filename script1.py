4a36ac02fd285782536960d7cdd5b2613ccba55a
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-07 23:58:57 +0530

    change ds

--- script1.py
+++ script1.py
@@ -4,11 +4,10 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
-import os
-
-# from sklearn.datasets import load_boston
 from sklearn.datasets import fetch_california_housing
-housing = fetch_california_housing()
 
-# boston_df = load_boston()
-print(housing.keys())
+X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+n_samples, n_features = X.shape
+
+
+print(f"The dataset consists of {n_samples} samples and {n_features} features")
