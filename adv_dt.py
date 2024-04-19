542802efcfe284e42176843a9587d1a3041f61c5
aleemsiddiqui0299 <86101375+aleemsiddiqui0299@users.noreply.github.com>
2024-04-19 22:37:55 +0530

    Create adv_dt.py

---
+++ adv_dt.py
@@ -0,0 +1,8 @@
+#File created for advanced decision tree pipeline to be used to fetch data from a stream / file 
+#Then model creation followed by model training happens and it will save the model in some compressed format
+
+
+#adding some neccessary imports
+import numpy as np
+import pandas as pd
+from sklearn.datasets import  fetch_openml
