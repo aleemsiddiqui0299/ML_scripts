54c8de0c3d0176fb6eea8c052a3e3db991b1c0fe
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-26 21:00:41 +0530

    async conversion

--- ai-backend/app.py
+++ ai-backend/app.py
@@ -2,13 +2,18 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+import asyncio
+import aiohttp
 
 app = Flask(__name__)
 
 client , db, collection = None, None, None
 
-def get_db_details():
+async def get_db_details():
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, get_db_details_async)
+
+async def get_db_details_async():
     try:
         client = MongoClient('')
         db = client['samle_mflix']
