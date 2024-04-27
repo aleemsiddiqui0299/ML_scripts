64c2486cb9112d0c4e9a498ca310c54bb859e36a
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-28 00:46:19 +0530

    data processing functions

--- ai-backend/test_sample_model.py
+++ ai-backend/test_sample_model.py
@@ -7,16 +7,6 @@ from sklearn.metrics import accuracy_score
 import joblib
 from tqdm import tqdm
 
-data = pd.read_csv('movies_data.csv')
-print(data.describe())
-
-X = data['review'][:1000]
-y = data['sentiment'][:1000]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-vectorizer = TfidfVectorizer(max_features=1000)
-X_train_features = vectorizer.fit_transform(X_train)
-X_test_features = vectorizer.transform(X_test)
 
 print(X_train_features.shape, X_test_features.shape)
 clf = RandomForestClassifier(n_estimators=100,random_state=42)
@@ -28,13 +18,22 @@ with tqdm(total=clf.n_estimators) as pbar:
 print("Model Training completed.")
 
 def load_data():
-    pass
+    data = pd.read_csv('movies_data.csv')
+    print(data.describe())
+    return data
 
 def split_data(data):
-    pass
+    X = data['review'][:1000]
+    y = data['sentiment'][:1000]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test 
+
 
 def extract_features(X_train, X_test):
-    pass
+    vectorizer = TfidfVectorizer(max_features=1000)
+    X_train_features = vectorizer.fit_transform(X_train)
+    X_test_features = vectorizer.transform(X_test)
+    return vectorizer, X_train_features, X_test_features
 
 def train_model(X_train, y_train):
     pass
