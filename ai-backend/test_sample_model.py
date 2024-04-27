be97e0993c97bf28dfa5bc70a57e312cbb8c1873
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-27 20:41:54 +0530

    test model gen file

---
+++ ai-backend/test_sample_model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
