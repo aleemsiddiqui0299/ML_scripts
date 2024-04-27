3560c3a8fc10de5ba1cf4da964d42154bc26190c
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-04-27 21:01:20 +0530

    text tfidf

--- ai-backend/test_sample_model.py
+++ ai-backend/test_sample_model.py
@@ -1,7 +1,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
+
+data = pd.read_csv('movies_data.csv')
+print(data.describe())
+
+X = data['review']
+y = data['sentiment']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+vectorizer = TfidfVectorizer(max_features=1000)
+X_train_features = vectorizer.fit_transform(X_train)
